b67c196030a7ba10671faee85468f90aa58e65ec
Lucas Colley <lucas.colley8@gmail.com>
2024-11-30 01:19:15 +0000

    vendor array-api-extra using script

---
+++ sklearn/externals/_array_api_extra/array_api_extra/__init__.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+from ._funcs import atleast_nd, cov, create_diagonal, expand_dims, kron, setdiff1d, sinc
+
+__version__ = "0.3.1"
+
+# pylint: disable=duplicate-code
+__all__ = [
+    "__version__",
+    "atleast_nd",
+    "cov",
+    "create_diagonal",
+    "expand_dims",
+    "kron",
+    "setdiff1d",
+    "sinc",
+]
